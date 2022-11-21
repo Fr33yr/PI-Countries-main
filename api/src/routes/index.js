1519d288467bbb3a2a666a283e3aabfbf9e39b61
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-11-21 19:13:28 -0300

    countries routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
+const { getCountries, getCountry } = require('../controllers/countries.controller')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+// Countries routes
+router.get('/countries', getCountries)
+router.get('/countries/:id', getCountry)
+// Activities routes
 
 module.exports = router;
