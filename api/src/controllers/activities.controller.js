b4fe872d92e8bb3fe8d85ba056dfa5b1d6f0d2b2
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-11-22 10:36:08 -0300

    activity controller

--- api/src/controllers/activities.controller.js
+++ api/src/controllers/activities.controller.js
@@ -1,2 +1,8 @@
 const { Activity } = require('../db.js');
+const axios = require('axios')
 
+const createActivity = (req, res) => {
+    const { name } = req.body
+}
+
+module.exports = { createActivity }
