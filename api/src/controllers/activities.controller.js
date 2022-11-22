b4d6c5b07c38b32ec82210fe6894558bb8b19997
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-11-22 10:46:41 -0300

    activity contoller validations

--- api/src/controllers/activities.controller.js
+++ api/src/controllers/activities.controller.js
@@ -1,8 +1,22 @@
 const { Activity } = require('../db.js');
-const axios = require('axios')
 
-const createActivity = (req, res) => {
-    const { name } = req.body
+const createActivity = async (req, res) => {
+    const { name, dificulty, duration, season } = req.body
+    try {
+        if(!name || !dificulty){
+            throw new Error('Este campo es requerido')
+        }else if(!duration || !season){
+            throw new Error('Este campo es requerido')
+        }
+        const newActivity = await Activity.create({
+            name,
+            dificulty,
+            duration,
+            season
+        })
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
 }
 
 module.exports = { createActivity }
