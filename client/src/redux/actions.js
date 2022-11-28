1fdd2bd31be76795cda07c81a804856d44423c97
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-11-28 17:41:45 -0300

    actions

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,22 +1,13 @@
 import axios from 'axios'
 import { GET_COUNTRIES, CREATE_ACTIVITY, GET_DETAILS } from './action.types'
 
-const getCountries = async (name) => {
-    return function (dispatch) {
-        axios.get(`http://localhost:3001/countries?name=${name}`)
-            .then(res => res.data)
-            .then(d => dispatch({ type: GET_COUNTRIES, payload: d }))
-            .catch(err => console.log(err))
-    }
-}
-
 const createActivity = async (props) => {
     return function (dispatch) {
         const { name, dificulty, duration, season } = props
         axios.post(`http://localhost:3001/activities`, {
             name, dificulty, duration, season
         }).then(function (response) {
-            dispatch({type: CREATE_ACTIVITY, payload: props})
+            dispatch({ type: CREATE_ACTIVITY, payload: props })
         })
             .catch(function (error) {
                 console.log(error);
@@ -24,13 +15,45 @@ const createActivity = async (props) => {
     }
 }
 
-const getDetails = (id) => async dispatch => {
-    try {
-        const response = await axios.get(`http://localhost:3001/countries/${id}`)
-        dispatch({type: GET_DETAILS, payload: response.data})
-    } catch (error) {
-        console.log(error);
+const getCountries = (countries) => {
+    return {
+        type: GET_COUNTRIES,
+        payload: countries
     }
 }
 
-export {getCountries, createActivity, getDetails}
+const getDetails = (details) => {
+    return {
+        type: GET_DETAILS,
+        payload: details
+    }
+}
+
+const fetchDetails = (id) => {
+    return function (dispatch) {
+        dispatch(getDetails())
+        axios.get(`http://localhost:3001/countries/${id}`)
+            .then(res => res.data)
+            .then(d => dispatch(getDetails(d)))
+            .catch(err => console.log(err))
+    }
+}
+
+const fetchCountries = (name) => {
+    return function (dispatch) {
+        dispatch(getCountries())
+        if (!name) {
+            axios.get(`http://localhost:3001/countries/`)
+                .then(res => res.data)
+                .then(d => dispatch(getCountries(d)))
+                .catch(res => console.log(res))
+        } else {
+            axios.get(`http://localhost:3001/countries?name=${name}`)
+                .then(res => res.data)
+                .then(d => dispatch(getCountries(d)))
+                .catch(res => console.log(res))
+        }
+    }
+}
+
+export { getCountries, createActivity, getDetails, fetchDetails, fetchCountries }
