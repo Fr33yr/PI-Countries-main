40fa7dc2319e176fcd5b5a25b3d474fd2038f469
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-11-28 01:39:39 -0300

    ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import Layout  from './components/layout/Layout'
+import Layout from './components/layout/Layout'
 import { Routes, Route } from 'react-router-dom'
-import {Home, Detail, Activity} from './pages/index'
+import { Home, Detail, Activity } from './pages/index'
 
 function App() {
 
   return (
     <Layout>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/country/:id' element={<Detail />}/>
-        <Route path='/activity' element={<Activity/>}/>
-      </Routes>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/country/:id' element={<Detail />} />
+          <Route path='/activity' element={<Activity />} />
+        </Routes>
+      </main>
     </Layout>
   )
 }
