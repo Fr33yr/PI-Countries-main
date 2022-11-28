1b263077ef33220ebe76a88907759213c0678d95
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-11-28 18:52:46 -0300

    store config fixed

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,12 @@
 import { legacy_createStore, applyMiddleware} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import rootReducer from './reducer'
-import thunkMiddleware from 'redux-thunk'
+import thunk from 'redux-thunk'
 
 const store = legacy_createStore(
   rootReducer,
   composeWithDevTools(
-    applyMiddleware(thunkMiddleware)
+    applyMiddleware(thunk)
   ),
 );
 
