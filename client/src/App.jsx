f73155af00eb719990c35f9539fe597b2a4dad21
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-11-29 08:37:52 -0300

    countries path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import Layout from './components/layout/Layout'
 import { Routes, Route } from 'react-router-dom'
-import { Home, Detail, Activity } from './pages/index'
+import { Home, Detail, Activity, Countries } from './pages/index'
 
 function App() {
 
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/country/:id' element={<Detail />} />
+          <Route path='/countries' element={<Countries />} />
           <Route path='/activity' element={<Activity />} />
         </Routes>
       </main>
