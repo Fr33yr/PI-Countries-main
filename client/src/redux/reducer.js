0825754fdb0e7bac1badd59312152df19c46d8e5
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-12-03 09:12:52 -0300

    ...

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,9 +1,6 @@
 import {
     GET_COUNTRIES, CREATE_ACTIVITY,
-    GET_DETAILS, ORDER_AZ,
-    ORDER_ZA, ORDER_MAX_TO_MIN,
-    ORDER_MIN_TO_MAX, FILTER_BY_CONTINENT,
-    FILTER_BY_ACTIVITY
+    GET_DETAILS
 } from './action.types'
 
 const initialState = {
@@ -19,47 +16,6 @@ export default (state = initialState, action) => {
                 ...state,
                 countries: [...action.payload]
             }
-        case ORDER_AZ:
-            if (state.countries <= 1) return { ...state }
-            return {
-                ...state,
-                countries: state.countries.sort((function (a, b) {
-                    if (a.name < b.name) {
-                        return -1;
-                    }
-                    if (a.name > b.name) {
-                        return 1;
-                    }
-                    return 0;
-                }))
-            }
-        case ORDER_ZA:
-            if (state.countries <= 1) return { ...state }
-            return {
-                ...state,
-                countries: state.countries.sort((function (a, b) {
-                    if (a.name > b.name) {
-                        return -1;
-                    }
-                    if (a.name < b.name) {
-                        return 1;
-                    }
-                    return 0;
-                }))
-            }
-        case ORDER_MAX_TO_MIN:
-            if (state.countries <= 1) return { ...state }
-            return {
-                ...state,
-                countries: state.countries.sort(function (a, b) { return b.population - a.population })
-            }
-        case ORDER_MIN_TO_MAX:
-            if (state.countries <= 1) return { ...state }
-            return {
-                ...state,
-                countries: state.countries.sort(function (a, b) { return a.population - b.population })
-            }
-        
         case GET_DETAILS:
             return {
                 ...state,
