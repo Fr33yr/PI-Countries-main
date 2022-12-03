2d49064c11aabc3f805f27ee8bf8233a98d4c456
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-12-03 10:23:38 -0300

    activity creation options

--- client/src/pages/activity/Activity.jsx
+++ client/src/pages/activity/Activity.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import styles from './activity.module.css'
 import { seasons as seasonsNames } from '../../utils/seasons'
 import Checkbox from '../../components/checkbox/Checkbox'
+import { getCountries } from '../../redux/actions'
 
 export default function Activity() {
     const [selectedSeason, setSelectedSeason] = useState({
         seasons: []
     })
     const [form, setForm] = useState({})
+    const [search, setSearch] = useState('')
+    const [countriesNames, setCountriesNames] = useState([])
+    const dispatch = useDispatch()
+    const countries = useSelector(state => state.countries)
+
+
+
 
     const handleChecks = (e) => {
         const { value, checked } = e.target
@@ -26,15 +35,27 @@ export default function Activity() {
         }
     }
 
+
     const handleChange = (e) => {
         const name = e.target.name;
         const value = e.target.value;
         setForm(values => ({ ...values, [name]: value }))
     }
 
+
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log({ ...form, ...selectedSeason })
+    }
+
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        search === "" ? [] : dispatch(getCountries(search))
+    }
+
+    const handleAddCountry = (name) => {
+        setCountriesNames(countriesNames.concat([name]))
     }
 
     return (
@@ -51,11 +72,29 @@ export default function Activity() {
                     min={1} max={99} required onChange={handleChange} />
                 <label>Temporada: </label>
                 {seasonsNames.map((s, index) => (
-                <Checkbox handleChecks={handleChecks} value={s} key={index}/>
+                    <Checkbox handleChecks={handleChecks} value={s} key={index} />
                 ))}
                 <label>Paises</label>
                 <button type="submit">Crear</button>
             </form>
+
+            <div className="searchcontainer">
+                <form onSubmit={handleSearch}>
+                    <input type="text" onChange={(e) => setSearch(e.target.value)} />
+                    <button type="submit" disabled={search === ""}>Find</button>
+                </form>
+                <div className="searchview">
+                    {
+                        countries.length > 0 && countries.map((c, index) => (
+                            <div className={styles.searchresult}>
+                                <p>{c.name}</p>
+                                <button onClick={()=>handleAddCountry(c.name)}
+                                disabled={countriesNames.includes(c.name)}>+</button>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </>
     )
 }
