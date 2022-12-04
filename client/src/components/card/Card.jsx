fe9c0ae36866b92ef1d607eb250767caf5fee2ac
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-12-04 17:47:35 -0300

    card

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import styles from './card.module.css'
 
 export default function Card(props) {
-    const {} = props
+    const {name, images, continent} = props
+    const flag = images[1]
   return (
     <>
         <div className={styles.card}>
-
+          <img src={flag} alt={`${name}-flag`} />
+          <h3>{name}</h3>
+          <h4>{continent}</h4>
         </div>
     </>
   )
