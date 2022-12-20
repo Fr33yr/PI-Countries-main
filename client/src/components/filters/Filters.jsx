8da5f7c82f0dddd777ae8dd5f053860a15e9436d
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-12-20 17:34:00 -0300

    filters

---
+++ client/src/components/filters/Filters.jsx
@@ -0,0 +1,27 @@
+import {useDispatch} from 'react-redux'
+
+import {sortBy} from '../../redux/actions'
+import styles from './filters.modulue.css'
+
+function Filters() {
+const dispatch = useDispatch()
+
+const handleSort = (e) => {
+    dispatch(sortBy(e.target.value))
+}
+
+    return (
+    <>
+        <div className={styles.filters}>
+            <fieldset onChange={handleSort}>
+                <input type="radio" name="sort" value={'A to Z'}/>
+                <input type="radio" name="sort" value={'Z to A'}/>
+                <input type="radio" name="sort" value={'morePopulation'}/>
+                <input type="radio" name="sort" value={'lessPopulation'}/>
+            </fieldset>
+        </div>
+    </>
+  )
+}
+
+export default Filters
