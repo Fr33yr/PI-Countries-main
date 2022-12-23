46fd4850c581dd76b141d6bdd14cba26b480f3a6
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-12-23 11:32:43 -0300

    get all activities controller

--- api/src/controllers/activities.controller.js
+++ api/src/controllers/activities.controller.js
@@ -41,4 +41,19 @@ const createActivity = async (req, res) => {
     }
 }
 
+const getAllActivities = async (req, res) => {
+    try {
+        const activities = await Activity.findAll({
+            include: Country
+        })
+        if(activities){
+            res.json(activities)
+        }else{
+            throw new Error('No activities found')
+        }
+    } catch (error) {
+        res.json({message: error.message})
+    }
+}
+
 module.exports = { createActivity }
