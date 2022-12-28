662125883989f32a3d8ceaaf7f1647f938767435
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-12-28 17:09:04 -0300

    text error

--- client/src/components/form/text/Text.jsx
+++ client/src/components/form/text/Text.jsx
@@ -1,17 +1,25 @@
+import { useState } from 'react'
 import styles from './text.module.css'
 
 function Text({ setForm }) {
-
+    const [error, setError] = useState('')
     const handleChange = (e) => {
         const value = e.target.value
         setForm(values => ({ ...values, name: value }))
     }
 
+    const handleError = (e) => {
+        const regEx = /^[A-Za-z\s]*$/
+        const value = e.target.value
+        regEx.test(value) ? setError("") : setError("Solo letras en este campo")
+    }
+
     return (
         <>
-            <label>Nombre: </label>
-            <input type="text" name='name'
-                required onChange={handleChange} autoComplete='off' />
+            <label>Nombre </label>
+            <input type="text" name='name' onSelect={handleError}
+                required onChange={handleChange} autoComplete='off'
+                className={error !== ''? styles.inputtext : ""} />
         </>
     )
 }
