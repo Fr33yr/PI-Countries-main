50159b73a8c6132f1dbe5117acc149d0302e5190
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-12-30 20:35:58 -0300

    name conflict error handled

--- api/src/controllers/activities/createActivity.js
+++ api/src/controllers/activities/createActivity.js
@@ -1,13 +1,23 @@
 const { Activity, Country } = require('../../db.js');
-const {Op} = require('sequelize')
+const { Op } = require('sequelize')
 
 // create activities
 const createActivity = async (req, res) => {
     const { name, dificulty, duration, season, countriesIds } = req.body
-    
+
     try {
-        if(!name || !dificulty || !duration || !season){
+        if (!name || !dificulty || !duration || !season) {
             throw new Error('Este campo es requerido')
+        }
+
+        const checkDuplicate = await Activity.findOne({
+            where:{
+                name
+            }
+        })
+
+        if (checkDuplicate !== null) {
+            throw new Error("Ya existe una actividad con ese nombre")
         }
 
         const activity = await Activity.create({
@@ -16,7 +26,7 @@ const createActivity = async (req, res) => {
             duration,
             season
         })
-        
+
         const options = []
 
         for (let i = 0; i < countriesIds.length; i++) {
@@ -25,6 +35,8 @@ const createActivity = async (req, res) => {
             })
         }
 
+
+
         const countries = await Country.findAll({
             where: {
                 [Op.or]: options
@@ -32,13 +44,16 @@ const createActivity = async (req, res) => {
             include: Activity
         })
 
-        countries.map((c) =>(
+        countries.map((c) => (
             c.addActivity(activity)
         ))
-            
-        res.status(201).json({message: "Todo salio bien"})
+
+        res.status(201).json({ message: "Todo salio bien" })
     } catch (error) {
-        return res.status(500).json({message: error.message})
+        if(error.message === "Ya existe una actividad con ese nombre"){
+            return res.status(409).json({message: error.message})
+        }
+        return res.status(500).json({ message: error.message })
     }
 }
-module.exports = {createActivity}
+module.exports = { createActivity }
