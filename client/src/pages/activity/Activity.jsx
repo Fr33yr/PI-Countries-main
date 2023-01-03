5edbbe4899f09a51473a502b171bd9a51ef2a375
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-03 17:23:56 -0300

    form icons styles

--- client/src/pages/activity/Activity.jsx
+++ client/src/pages/activity/Activity.jsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { faStar as solidFaStar } from '@fortawesome/free-solid-svg-icons'
+
 import styles from './activity.module.css'
 import { createActivity } from '../../redux/actions'
-import {
-    Search,
-    FormError
-} from '../../components/index'
+import { Search, FormError } from '../../components/index'
 import { seasons } from '../../utils/seasons'
 
 export default function Activity() {
-    const formInitialState = { name: '', dificulty: 1, duration: 1, season: '' }
+    // === Local state ===
+    const formInitialState = { name: '', dificulty: 0, duration: 1, season: '' }
     const [formValues, setFormValues] = useState({ ...formInitialState })
     const [countriesIds, setCountriesIds] = useState([])
     const [isDisabled, setIsDisabled] = useState(false)
@@ -23,10 +25,10 @@ export default function Activity() {
         }
     }, [formValues])
 
-    //selectors
+    // === Selectors ===
     const actErr = useSelector(state => state.activity)
 
-    //handlers
+    // === Handlers ===
     const handleChange = (e) => {
         const value = e.target.value
         const name = e.target.name
@@ -47,28 +49,44 @@ export default function Activity() {
             {actErr.message ? <FormError text={actErr.message} /> : ""}
             <div className={styles.activities}>
                 <form className={styles.activityform} onSubmit={handleSubmit}>
+
                     {/* === Name input === */}
-                    <label htmlFor="name">Nombre: </label>
+                    <label htmlFor="name">Nombre </label>
                     <input type="text" name="name"
                         onChange={handleChange} autoComplete={'off'} value={formValues.name} />
+
                     {/* === Duration input === */}
-                    <label htmlFor="">Duracion: </label>
-                    <input type="number" name='age' value={formValues.duration}
+                    <label htmlFor="duration">Duracion </label>
+                    <input type="number" name='duration' value={formValues.duration}
                         onChange={handleChange} />
+
                     {/* === Dificulty input === */}
-                    <label htmlFor="">Dificultad</label>
-                    {Array(5).fill().map((v, i) => 1 + i).map((item, index) => (
-                        <button key={`dificulty-button-${index}`}>{item}</button>
-                    ))}
+                    <label htmlFor="dificulty">Dificultad</label>
+                    <div className={styles.dificultyinput}>
+                        {Array(5).fill().map((v, i) => 1 + i).map((item, index) => (
+                            <button key={`dificulty-button-${index}`} type='button'
+                                onClick={() => setFormValues({
+                                    ...formValues,
+                                    dificulty: item
+                                })} className={styles.starbtn}>
+                                <FontAwesomeIcon icon={solidFaStar} fixedWidth size='sm' stroke='full' style={{ color: 'yellow' }} />
+                            </button>
+                        ))}
+                    </div>
+
                     {/* === Season input === */}
                     <label htmlFor="">Temporada</label>
-                    <select name="" value={formValues.season}>
+                    <select name='season' value={formValues.season}
+                        onChange={handleChange} className={styles.seasoninput}>
                         {seasons.map((season, index) => (
-                            <option value={season} key={`season-option-${index}`}
+                            <option value={season}
+                                key={`season-option-${index}`}
                             >{season}</option>
                         ))}
                     </select>
-                    <button type="submit" disabled={isDisabled}>Crear</button>
+
+                    <button type="submit" disabled={isDisabled}
+                        className={styles.submitbtn}>Crear</button>
                 </form>
                 <Search countriesIds={countriesIds}
                     setCountriesIds={setCountriesIds}
