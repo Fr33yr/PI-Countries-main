811add5c486871679976bb4b4ae92f9c1d2ab052
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-04 17:21:00 -0300

    search

--- client/src/pages/countries/Countries.jsx
+++ client/src/pages/countries/Countries.jsx
@@ -3,39 +3,37 @@ import { useDispatch, useSelector } from 'react-redux'
 import styles from './countries.module.css'
 
 import { getCountries, getAllActivities } from '../../redux/actions'
-import {Filters, Card} from '../../components/index'
+import { Filters, Card } from '../../components/index'
 
 
 export default function Countries() {
     const [name, setName] = useState('')
     const dispatch = useDispatch()
 
-    
+
+    useEffect(() => {
+        dispatch(getAllActivities())
+        if (name !== undefined || name !== '') {
+            dispatch(getCountries(name))
+        } else {
+            dispatch(getCountries())
+        }
+    }, [name])
+
+
     //selectors
     const filteredCountries = useSelector(state => state.filteredCountries)
     const countries = useSelector(state => state.countries)
 
-    useEffect(()=>{
-        dispatch(getAllActivities())
-        if(!countries || countries.length <= 0){
-            if(name !== undefined || name !== ''){
-                dispatch(getCountries(name))
-            }else{
-                dispatch(getCountries())
-            }
-        }
-    },[name])
 
-
-    
     return (
         <>
-            <Filters name={name} setName={setName}/>
+            <Filters name={name} setName={setName} />
             <div className={styles.countriescontainer}>
                 {filteredCountries.length > 0 &&
-                filteredCountries.map((item,index) => (
-                    <Card {...item} key={item.id}/>
-                ))}
+                    filteredCountries.map((item, index) => (
+                        <Card {...item} key={item.id} />
+                    ))}
             </div>
         </>
     )
