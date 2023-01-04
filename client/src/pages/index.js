bfa7c460b3d542c67b90a7a1faeb8416ecedc878
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-04 17:21:09 -0300

    barrel update

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,4 @@
-export { default as Activity } from './activity/Activity';
+export { default as AddActivity } from './activity/AddActivity';
 export { default as Countries } from './countries/Countries';
 export { default as Detail } from './detail/Detail';
 export { default as Home } from './home/Home';
