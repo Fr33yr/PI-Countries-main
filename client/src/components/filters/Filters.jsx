4a7b1f9e1f2b916791985170cfae35704d746674
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-08 17:21:56 -0300

    filter by activity and comments

--- client/src/components/filters/Filters.jsx
+++ client/src/components/filters/Filters.jsx
@@ -1,5 +1,5 @@
 import styles from './filters.module.css'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { sortBy, filterBy } from '../../redux/actions'
 import {continentOptions, sortfiltersOptions} from '../../utils/options'
@@ -21,16 +21,29 @@ function Filters({name, setName}) {
         setName(value)
     }
 
+    // === Selectors ===
+    const activities = useSelector(state => state.activities)
+
     return (
         <>
             <div className={styles.filters}>
+                {/* === Name input === */}
                 <input type="text" name="name" onChange={handleName} autoComplete='off'/>
+                {/* === Filter by continent === */}
                 <select name="contients" onChange={handleFilterByContinent}>
                     {continentOptions.map((c,index) => (
                         <option value={c} key={index}>{c}</option>
                     ))}
                 </select>
+                {/* === Filter by activity === */}
+                <select name="activities">
+                    {activities.length > 0 ? activities.map((activity) => (
+                        <option key={`activity-${activity.id}`}>{activity.name}</option>
+                    )) : <option>---</option>}
+                </select>
+                {/* === Sort options === */}
                 <fieldset onChange={handleSort}>
+                    <legend>Sort by</legend>
                     {sortfiltersOptions.map((item, index) => {
                         return(
                             <>
