7292de8d293b2bd49c9b993ed2160d8bc7ece53a
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-11 20:01:23 -0300

    ..

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { getApiData } = require('./src/controllers/contries/getApiData.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   getApiData()
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
