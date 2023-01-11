42f33514845429772f02ddcc9469f49b8be3a1f2
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-10 23:34:46 -0300

    alter

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { getApiData } = require('./src/controllers/contries/getApiData.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ alter: false }).then(() => {
   getApiData()
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
