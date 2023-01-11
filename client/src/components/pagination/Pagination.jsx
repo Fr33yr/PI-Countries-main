c349d47610a12e9c84c81a44b6fa1ed75b724998
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-10 23:35:54 -0300

    pagination implemented

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -1,18 +1,36 @@
-import { useState, useEffect } from 'react'
 import styles from './pagination.module.css'
 
-function Pagination() {
+function Pagination({ currentIndex, setCurrentIndex, nPages }) {
     // === Local state ===
-    const [isDisabled, setIsDisabled] = useState(false)
+    let prevPage = currentIndex - 1
+    let nextPage = currentIndex + 1
 
+    // === handlers ===
+    const handlePrev = () => {
+        setCurrentIndex(currentIndex - 1)
+    }
+    const handleNext = () => {
+        setCurrentIndex(currentIndex + 1)
+    }
+    const handleFist = () => {
+        setCurrentIndex(0)
+    }
+    const handleLast = () => {
+        setCurrentIndex(nPages)
+    }
+    
     return (
         <>
             <div className={styles.pagination}>
-                <button disabled={isDisabled}>Prev</button>
-                <p>prevPage</p>
-                <p>currentPage</p>
-                <p>nextPage</p>
-                <button disabled={isDisabled}>Next</button>
+                <button onClick={handleFist} disabled={nPages === 0 || currentIndex === 0}>First</button>
+                <button  onClick={handlePrev} disabled={currentIndex === 0 || nPages === 0}
+                >Prev</button>
+                <p>{prevPage >= 0 ? prevPage : ""}</p>
+                <p className={styles.currentPage}>{currentIndex}</p>
+                <p>{nextPage > nPages || nPages === 0 ? "" : nextPage}</p>
+                <button  onClick={handleNext} disabled={currentIndex === nPages || nPages === 0}
+                >Next</button>
+                <button onClick={handleLast} disabled={nPages === 0 || currentIndex === nPages}>Last</button>
             </div>
         </>
     )
--- client/src/pages/countries/Countries.jsx
+++ client/src/pages/countries/Countries.jsx
@@ -4,19 +4,26 @@ import styles from './countries.module.css'
 
 import { getCountries, getAllActivities, resetError } from '../../redux/actions'
 import { Filters, Card, PageError, Pagination } from '../../components/index'
+import { Paginate } from '../../utils/paginate'
 
 
 export default function Countries() {
+    // === Local state ===
     const [name, setName] = useState('')
+    const [currentIndex, setCurrentIndex] = useState(0)
     const dispatch = useDispatch()
-
+    
+    const cardsPerPage = 10
+    const cardsFirstPage = 9
 
     useEffect(() => {
         dispatch(resetError())
         dispatch(getAllActivities())
         if (name !== undefined || name !== '') {
+            setCurrentIndex(0)
             dispatch(getCountries(name))
         } else {
+            setCurrentIndex(0)
             dispatch(getCountries())
         }
     }, [name])
@@ -25,20 +32,29 @@ export default function Countries() {
     //selectors
     const filteredCountries = useSelector(state => state.filteredCountries)
     const err = useSelector(state => state.error)
-console.table(filteredCountries);
+
+    let nPages = Array.isArray(Paginate(filteredCountries, cardsPerPage, cardsFirstPage)[0]) ? 
+    Paginate(filteredCountries, cardsPerPage, cardsFirstPage).length - 1 : 0
 
     return (
         <>
-            <Filters name={name} setName={setName} />
+            <Filters name={name} setName={setName} setCurrentIndex={setCurrentIndex}/>
             {err.code !== '' ? <PageError text={err.code} /> : ''}
-            {filteredCountries.length > 0 ? <Pagination /> : ""}
+            {/* === Pagination === */}
+            {<Pagination currentIndex={currentIndex}
+                setCurrentIndex={setCurrentIndex}  nPages={nPages}/>}
+            {/* === Cards === */}
             <div className={styles.countriescontainer}>
-                {filteredCountries.length > 0 &&
-                    filteredCountries.map((item) => (
-                        <Card {...item} key={`card-${item.id}`} />
-                    ))}
+                {nPages > 0 ? Paginate(filteredCountries, cardsPerPage, cardsFirstPage)[currentIndex].map((country) => (
+                    <Card {...country} key={country.id}/>
+                )) : Paginate(filteredCountries, cardsPerPage, cardsFirstPage).map((country) => (
+                    <Card {...country} key={country.id}/>
+                ))
+                }
             </div>
-            {filteredCountries.length > 0 ? <Pagination /> : ""}
+            {/* === Pagination === */}
+            {<Pagination currentIndex={currentIndex}
+                setCurrentIndex={setCurrentIndex}  nPages={nPages}/>}
         </>
     )
 }
