27162723fa0d36c2adbea6592e1c229ed32c55ff
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-14 15:49:47 -0300

    filters local state

--- client/src/components/filters/Filters.jsx
+++ client/src/components/filters/Filters.jsx
@@ -3,24 +3,30 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import { sortBy, filterBy, resetFilters } from '../../redux/actions'
 import {continentOptions, sortfiltersOptions} from '../../utils/options'
+import { useState } from 'react'
 
 function Filters({setName, setCurrentIndex}) {
+    const initialFilterValues = {sort: "", continent: "", activity:"", name: ""}
+    const [filterValues, setFilterValues] = useState(initialFilterValues)
     const dispatch = useDispatch()
 
     // === Handlers ===
     const handleSort = (e) => {
         setCurrentIndex(0)
         const value = e.target.value
+        setFilterValues({...filterValues, sort: value})
         dispatch(sortBy(value))
     }
     const handleFilterByContinent = (e) => {
         setCurrentIndex(0)
         const value = e.target.value
+        setFilterValues({...filterValues, continent: value})
         dispatch(filterBy(value, null))
     }
     const handleFilterByActivity = (e) => {
         setCurrentIndex(0)
         const value = e.target.value
+        setFilterValues({...filterValues, activity: value})
         dispatch(filterBy(null, value))
     }
     const handleName = (e) => {
@@ -42,12 +48,14 @@ function Filters({setName, setCurrentIndex}) {
                 <input type="text" name="name" onChange={handleName} autoComplete='off'/>
                 {/* === Filter by continent === */}
                 <select name="contients" onChange={handleFilterByContinent}>
+                    <option value="">select a continent</option>
                     {continentOptions.map((c,index) => (
                         <option value={c} key={`${index}-contient-option`}>{c}</option>
                     ))}
                 </select>
                 {/* === Filter by activity === */}
                 <select name="activities" onChange={handleFilterByActivity}>
+                    <option value="">select an activity</option>
                     {activities.length > 0 ? activities.map((activity) => (
                         <option key={`activity-${activity.id+1}`}
                         value={activity.name}>{activity.name}</option>
